e64845b3317c951aaea4084d4f35dd6e929b4c5b
tvaddi <tvaddi@INHYNTVADDI01>
2022-09-09 14:14:58 +0530

    InicialFiles

---
+++ ApiProject_Sep9Fri/ApiProject_Sep9Fri/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ApiProject_Sep9Fri
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
